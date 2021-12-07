8d058d73dd2c8f4347bda02aa90c88054c073f05
Manuela <ellaheckenberger@gmail.com>
2021-12-07 10:32:33 +0100

    Punkt 1

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,10 +1,17 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
-    
+        // test --> Referenzvaribale / RAM- Adresse des Objekts/Instanz
+        // Test --> Referenztype ( reference type) // Sicht von außen
+        // <--> Instanz // sicht von innen
+
+        Test test = new Test();
+        System.out.println("Referenzvariable: + test");
+        test.showAddresse();
+
     }
 
 }
---
+++ src/com/cc/java/Test.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Test {
+
+    void showAddresse() {
+        System.out.println("Insranzvariable :" + this);
+
+        // this --> Instanzvariable verweist aus der Instanz auf sich selbst
+    }
+
+}
